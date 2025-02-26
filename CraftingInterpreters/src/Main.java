1702e12addf7da8d69e48d222c573d262e52b53a
eric.farias <eric.farias@SKYL-F5ZKCK3.choicehotels.com>
2025-02-25 18:31:34 -0700

    First Git Commit Of The Project

---
+++ CraftingInterpreters/src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.Out.Println("This is the first git commit of the project");
+
+	}
+
+}
